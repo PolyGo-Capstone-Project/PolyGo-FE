f54c1e14aaa80dca922503cef7ea033b4e329b1d
peterxyjz <huyq09@gmail.com>
2025-10-12 23:10:45 +0700

    feat: done manage user

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,9 +1,13 @@
 import z from "zod";
 
 import { Gender, MeritLevel } from "@/constants";
+import {
+  PaginationLangQuerySchema,
+  PaginationMetaSchema,
+} from "@/models/common.model";
 
 export const UserSchema = z.object({
-  id: z.number(),
+  id: z.string(),
   mail: z.email().nonempty().max(100),
   name: z.string().min(1).max(100),
   password: z.string().min(6).max(100).nonempty(),
@@ -17,6 +21,7 @@ export const UserSchema = z.object({
   isNew: z.boolean().default(true),
   role: z.string(),
   introduction: z.string().max(500).nullable(),
+  balance: z.number().min(0).default(0),
   deletedAt: z.iso.datetime().nullable(),
   createdAt: z.iso.datetime(),
   lastLoginAt: z.iso.datetime(),
@@ -27,7 +32,6 @@ export const GetUserProfileResSchema = z.object({
   data: UserSchema.omit({
     password: true,
     totp: true,
-    autoRenewSubscription: true,
   }),
 });
 
@@ -49,10 +53,51 @@ export const SetupProfileBodySchema = z.object({
 // PUT /users/profile/me update profile
 export const UpdateProfileBodySchema = SetupProfileBodySchema;
 
-// PUT /users/set-restrictions
-export const SetRestrictionsBodySchema = UserSchema.pick({
-  meritLevel: true,
+// ADMIN ==============
+
+// PUT /users/set-restriction
+export const SetRestrictionsBodySchema = z.object({
+  id: z.string(),
+  meritLevel: z.enum(Object.values(MeritLevel) as [string, ...string[]]),
 });
+
+// Get All Users
+export const UserListItemSchema = UserSchema.pick({
+  id: true,
+  name: true,
+  mail: true,
+  avatarUrl: true,
+  meritLevel: true,
+  role: true,
+  gender: true,
+  experiencePoints: true,
+  autoRenewSubscription: true,
+  streakDays: true,
+  isNew: true,
+  balance: true,
+  lastLoginAt: true,
+});
+
+export const GetUsersQuerySchema = PaginationLangQuerySchema.omit({
+  lang: true,
+});
+
+export const GetUsersResSchema = z.object({
+  data: z.object({
+    items: z.array(UserListItemSchema),
+    ...PaginationMetaSchema.shape,
+  }),
+  message: z.string(),
+});
+
+export const GetUserByIdBodySchema = UserSchema.pick({ id: true });
+
+export const GetUserByIdResSchema = z.object({
+  data: UserListItemSchema,
+  message: z.string(),
+});
+
+// ===================
 
 //types
 export type UserType = z.infer<typeof UserSchema>;
@@ -61,3 +106,8 @@ export type UpdateMeBodyType = z.infer<typeof UpdateMeBodySchema>;
 export type SetupProfileBodyType = z.infer<typeof SetupProfileBodySchema>;
 export type UpdateProfileBodyType = z.infer<typeof UpdateProfileBodySchema>;
 export type SetRestrictionsBodyType = z.infer<typeof SetRestrictionsBodySchema>;
+export type UserListItemType = z.infer<typeof UserListItemSchema>;
+export type GetUsersQueryType = z.infer<typeof GetUsersQuerySchema>;
+export type GetUsersResType = z.infer<typeof GetUsersResSchema>;
+export type GetUserByIdBodyType = z.infer<typeof GetUserByIdBodySchema>;
+export type GetUserByIdResType = z.infer<typeof GetUserByIdResSchema>;
