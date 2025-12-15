45603c0605b65a466c993674234cd1370d8276e4
Dien Nguyen Hoang <diennguyenhoang2902@gmail.com>
2025-12-15 22:31:14 +0700

    feat: delete file noti

--- src/components/shared/layouts/user/index.ts
+++ src/components/shared/layouts/user/index.ts
@@ -1,4 +1,3 @@
 export * from "./notification";
-export * from "./notification-key";
 export * from "./user-header";
 export * from "./user-menu";
--- src/components/shared/layouts/user/notification-key.tsx
+++
@@ -1,6 +0,0 @@
-// notification.keys.ts
-export const notificationKeys = {
-  all: (lang: string) => ["notifications", lang] as const,
-  list: (lang: string, params: { pageNumber: number; pageSize: number }) =>
-    [...notificationKeys.all(lang), "list", params] as const,
-};
